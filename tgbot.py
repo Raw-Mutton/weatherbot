75a57eb5de2d52655dc5f024b276d3969478afae
Veikka Niemi <veikka.niemi@aalto.fi>
2023-11-22 20:14:13 +0200

    Add start functionality to bot

--- tgbot.py
+++ tgbot.py
@@ -1,6 +1,23 @@
+import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import config
 
+logging.basicConfig(
+    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level = logging.INFO
+)
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    ???
+    await context.bot.send_message(
+        chat_id = update.effective_chat.id,
+        text = "Let's get you some weather info"
+    )
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(config.api_key).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.run_polling
