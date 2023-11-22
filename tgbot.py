821638abae08c9fc00e360d4da8b668b03657e6b
Veikka Niemi <veikka.niemi@aalto.fi>
2023-11-22 20:17:29 +0200

    Change token to string

--- tgbot.py
+++ tgbot.py
@@ -15,7 +15,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(config.api_key).build()
+    application = ApplicationBuilder().token(str(config.api_key)).build()
 
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
