96b1a510353d5c7a9c98f936d953818d703c77e1
Veikka Niemi <veikka.niemi@aalto.fi>
2023-11-30 14:34:19 +0200

    Create first version of bot's temperature command

--- tgbot.py
+++ tgbot.py
@@ -3,6 +3,8 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import config
 
+import weather
+
 logging.basicConfig(
     format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level = logging.INFO
@@ -14,10 +16,17 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         text = "Let's get you some weather info"
     )
 
+async def temperature(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    #current_temperature = weather.get_temperature()
+    await update.message.reply_text(weather.get_temperature())
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(str(config.api_key)).build()
 
     start_handler = CommandHandler('start', start)
+    temperature_handler = CommandHandler('temperature', temperature)
+
     application.add_handler(start_handler)
+    application.add_handler(temperature_handler)
 
     application.run_polling()
