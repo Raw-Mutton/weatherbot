610a508c82ac4733a1e4fc030a7b8ba5e6bd3606
Veikka Niemi <veikka.niemi@aalto.fi>
2023-11-30 11:50:26 +0200

    Figuring out get_current_-data() function

--- weather.py
+++ weather.py
@@ -4,16 +4,32 @@ import json
 
 key = config.weather_api
 
-parameters = {
-    "key": key,
-    "q": "Helsinki",
-    "aqi": "no"
-}
+def get_current_data():
+    try:
+        url = "http://api.weatherapi.com/v1/current.json"
+        parameters = {
+            "key": key,
+            "q": "Helsinki",
+            "aqi": "no"
+        }
+        response = requests.get(url, params=parameters)
 
-url = "http://api.weatherapi.com/v1/current.json"
-response = requests.get(url, params=parameters)
+        if response.status_code == 200:
+            print("Successfully retrieved data")
+            data = response.json()
+            return data
+        else:
+            print(f"Error: {response.status_code}")
+            return None
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Requests exception: {e}")
+        return None
 
-print(response.json())
 
 # def get_temperature():
-#     call = ???
+#     data = get_data()
+#     data["current"]
+
+data = get_data()
+print(data)
