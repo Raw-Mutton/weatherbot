3890142b44976052f5888aea0f6cf39da6f49820
Veikka Niemi <veikka.niemi@aalto.fi>
2024-01-25 20:56:30 +0200

    Create get_history(), get_temperature()

--- plotter.py
+++ plotter.py
@@ -1,3 +1,38 @@
 # Functions to plot the weather forecast and history
 
 import matplotlib.pyplot as plt
+import config
+import requests
+
+key = config.weather_api
+
+def get_history(time):
+    try:
+        url = "http://api.weatherapi.com/v1/history.json"
+        parameters = {
+            "key": key,
+            "q": "Helsinki",
+            "dt": time
+        }
+        response = requests.get(url, params=parameters)
+
+        if response.status_code == 200:
+            print("Successfully retrieved data")
+            data = response.json()
+            return data
+        else:
+            print(f"Error: {response.status_code}")
+            return None
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Requests exception: {e}")
+        return None
+    
+def get_temperatures(time):
+    data = get_history(time)
+    if data is None:
+        print("Failed to retrieve API data")
+
+    else:
+        forecast = data["forecast"]["forecastday"]
+        hourly = forecast["hour"]
